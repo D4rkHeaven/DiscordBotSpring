15a29fd3f7f3e4af83943bb3c354679be8474468
LopatinDmitrii <l.dmi3.v@gmail.com>
2020-08-10 19:38:30 +0400

    Add modules, dependencies and config

---
+++ bot-impl/src/main/java/bot/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package bot.config;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.security.auth.login.LoginException;
+import java.util.List;
+
+@Configuration
+public class ApplicationConfig {
+    @Value("${bot.token}")
+    private String token;
+
+    @Bean
+    public JDA jda(List<ListenerAdapter> listenerAdapters) throws LoginException {
+        JDA jda = JDABuilder.createDefault(token)
+                .setActivity(Activity.listening("!help"))
+                .build();
+        jda.addEventListener(listenerAdapters.toArray());
+        return jda;
+    }
+}
