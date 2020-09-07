3a3e36d32fd9042986ba3461af2885eac238a72b
LopatinDmitrii <l.dmi3.v@gmail.com>
2020-09-07 10:38:06 +0400

    Delete hibernate dependency

--- bot-impl/src/main/java/bot/config/JdaConfig.java
+++ bot-impl/src/main/java/bot/config/JdaConfig.java
@@ -1,17 +1,24 @@
 package bot.config;
 
+import bot.repository.UserRepository;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.security.auth.login.LoginException;
 import java.util.List;
 
 @Configuration
+@EnableTransactionManagement
+@EntityScan(basePackages = {"bot.entity"})
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class JdaConfig {
 
     @Value("${bot.token}")
